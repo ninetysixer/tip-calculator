07355bc1cc1249c2f5c2b3725b9d69310be49e7e
Semih Dursungül <114025283+semihdursungul@users.noreply.github.com>
2023-05-22 19:25:03 +0300

    Update main.py

--- spotifyTimeMachine/main.py
+++ spotifyTimeMachine/main.py
@@ -4,7 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # Scraping Billboard 100
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+date = input("Type in which date you want to travel to in YYYY-MM-DD format: ")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.select("li ul li h3")
@@ -26,14 +26,14 @@ user_id = sp.current_user()["id"]
 print(user_id)
 
 # Searching Spotify for songs by title
-song_uris = []
+song_uri = []
 year = date.split("-")[0]
 for song in song_names:
     result = sp.search(q=f"track:{song} year:{year}", type="track")
     print(result)
     try:
         uri = result["tracks"]["items"][0]["uri"]
-        song_uris.append(uri)
+        song_uri.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
@@ -42,4 +42,4 @@ playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", p
 print(playlist)
 
 # Adding songs found into the new playlist
-sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uri)
